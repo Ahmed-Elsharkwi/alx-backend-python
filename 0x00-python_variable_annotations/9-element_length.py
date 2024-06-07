3e2dbd5f0b5edcd2ed68e456064de50d51a8a530
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-07 23:17:30 +0300

    MM

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+return sum_list which takes a list input_list of floats as argument and
+returns their sum as a float.
+"""
+from typing import List, Union, Tuple, Callable, Iterator, Sequence
+
+
+def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
