2bcff35ac273fbbc0c752ada223a9cc30d153cb5
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-07 17:43:42 +0300

    MMM

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+get the n float number and return the string representation of the 
+float
+"""
+
+
+def to_str(n: float) -> str:
+    """ get the n float number and return string representation"""
+    return str(n)
