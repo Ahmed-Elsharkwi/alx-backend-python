c993c2bb86c12cad2c084930a21b96b68a913bde
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-11 21:30:10 +0300

    MM

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''that is okay '''
+from typing import List
 import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ comprehension """
     result = []
     async for i in async_generator():
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """ measure time """
     st = time.time()
     coros = [async_comprehension() for i in range(4)]
