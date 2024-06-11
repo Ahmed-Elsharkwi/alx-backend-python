501f53348b5148bb82f8e0da01a92cf051af5682
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-11 17:28:23 +0300

    MM

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" return list of number """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the dely time for each function """
+    new_list = []
+
+    for i in range(0, n):
+        result = task_wait_random(max_delay)
+        delay = await result
+
+        for idx in range(0, len(new_list)):
+            if new_list[idx] > delay:
+                temp = new_list[idx]
+                new_list[idx] = delay
+                delay = temp
+        new_list.append(delay)
+
+    return new_list
