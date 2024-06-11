ece5e190df669265a5cb05f56be8f7188ed5fa3c
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-11 21:23:37 +0300

    MM

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+measure time
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ measure time """
+    st = time.time()
+    coros = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*coros)
+    et = time.time()
+    return et - st
