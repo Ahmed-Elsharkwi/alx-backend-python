ad7bc997ecdff2385fd58ceca07f99923dabf7cf
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-05-21 17:57:09 +0300

    add generator streams row from an sql database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+seed = __import__('seed')
+
+
+def stream_users():
+    """ fetch the data and return it using yield """
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM user_data;")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        new_dict = {
+            "user_id": row[0],
+            "name": row[1],
+            "email": row[2],
+            "age": row[3]
+        }
+        yield new_dict
+
+    cursor.close()
