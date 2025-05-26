7b7630f85300ea152d426af0fd2dbfad39d57f01
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-05-26 18:05:32 +0300

    change the name of the function

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,12 +5,15 @@ import aiosqlite
 async def async_fetch_users(cursor):
     """ fetch all users """
     cur = await cursor.execute("SELECT * FROM users")
-    print(await cur.fetchall())
+    result = await cur.fetchall()
+    return result
+
 
 async def async_fetch_older_users(cursor):
     """ fetch all users older than 40 """
     cur = await cursor.execute("SELECT * FROM users WHERE age > 40")
-    print(await cur.fetchall())
+    result = await cur.fetchall()
+    return result
 
 async def fetch_concurrently():
     async with aiosqlite.connect("users.db") as db:
