c2d512a20c205487bc1c209ec831f51ccc155dd2
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-05-28 23:08:39 +0300

    add test memo

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """ module for testing access_nested_map """
 from flask import jsonify
 import unittest
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple, Union, Callable
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
@@ -66,3 +66,33 @@ class TestGetJson(unittest.TestCase):
             result = get_json(url)
 
         self.assertEqual(result, payload)
+
+class TestMemoize(unittest.TestCase):
+    """ class for testing memoziation """
+    def test_memoize(self) -> None: 
+        """ test memoization """
+        class TestClass:
+            """Test class """
+
+            def a_method(self) -> int:
+                return 42
+
+            @memoize
+            def a_property(self) -> int:
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_get:
+            mock_get.return_value = 42
+            obj = TestClass()
+            result_1 = obj.a_property()
+            result_2 = obj.a_property()
+
+            mock_get.assert_called_once_with()
+
+        self.assertEqual(result_1, 42)
+        self.assertEqual(resutl_2, 42)
+
+    
+
+
+
