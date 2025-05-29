8c5ded5e5b8a31c690716657dcc42f2be0d6b951
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-05-29 19:53:58 +0300

    add test to public repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,24 +2,24 @@
 """ module for testing client """
 import unittest
 from typing import Dict, Tuple, Union, Callable
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ class for testing org function """
-    @parameterized.expand([
-         ("google"),
-         ("abc"),
-         ])
+    """class for testing org function"""
+
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
     @patch("client.get_json")
-    def test_org(
-            self,
-            url: str,
-            mock_get: Callable) -> None:
-        """test the org function """
+    def test_org(self, url: str, mock_get: Callable) -> None:
+        """test the org function"""
         response = {"payload": True}
         mock_get.return_value = response
 
@@ -27,3 +27,19 @@ class TestGithubOrgClient(unittest.TestCase):
         result = object_1.org
 
         self.assertEqual(result, response)
+
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
+    def test_public_repos_url(self, url: str) -> None:
+        """test the puplic repos_url"""
+        with patch(
+                "client.GithubOrgClient.org", new_callable=PropertyMock)as m:
+            result = {"repos_url": True}
+            m.return_value = result
+
+            obj = GithubOrgClient(url)
+            self.assertEqual(obj._public_repos_url, True)
