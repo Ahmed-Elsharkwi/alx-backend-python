6766b685c4a0d480f90e95a93c1146856a38d283
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-06-05 18:18:18 +0300

    add authenication to chat app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -54,8 +54,9 @@ ROOT_URLCONF = 'messaging_app.urls'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
