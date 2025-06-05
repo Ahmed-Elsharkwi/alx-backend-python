d7367f80d954008e43af41c1bb4ec23d9c3ce68a
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-06-05 19:56:05 +0300

    add the admin application

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "rest_framework_simplejwt",
-    "chats"
+    "chats",
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
