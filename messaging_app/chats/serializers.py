69558fa95a2c7ac528875541f86281ede7583c84
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2025-06-06 21:30:18 +0300

    add the serializered classes

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,26 @@
+""" serializers module """
+from rest_framework import serializers
+from chats.models import User, Conversation, message
+
+
+class UserSerializer(serializer.ModelSerializer):
+    """ user serializer class """
+    class Meta:
+
+        model = User
+        fields = '__all__'
+
+class MessageSerializer(serializer.ModelSerializer):
+    """ Message serializer class """
+    class Meta:
+        model = Message
+        field = '__all__'
+
+class ConversationSerializer(serializer.ModelSerializer):
+    """ conversation serializer class """
+    message = MessageSerializer()
+
+    class Meta:
+
+        model = Conversation
+        field = '__all__'
